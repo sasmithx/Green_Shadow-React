b4cd1145c909af3a97d9977f260689f36ef8b465
Sasmith Manawadu <sasmithgit@gmail.com>
2025-01-16 13:13:30 +0530

    Store Improved

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,18 +1,34 @@
-import {configureStore} from "@reduxjs/toolkit";
+// import {configureStore} from "@reduxjs/toolkit";
+//
+// // import EquipmentSlice from "../reducers/EquipmentSlice.ts";
+// // import FieldSlice from "../reducers/FieldSlice.ts";
+// import VehicleSlice from "../reducers/VehicleSlice.ts";
+// import StaffSlice from "../reducers/StaffSlice.ts";
+// import CropSlice from "../reducers/CropSlice.ts";
+//
+// export const store = configureStore({
+//     reducer: {
+//         // crop: CropSlice,
+//         // equipment: EquipmentSlice,
+//         // field: FieldSlice,
+//         crop: CropSlice,
+//         vehicle: VehicleSlice,
+//         staff: StaffSlice,
+//     }
+// });
 
-// import EquipmentSlice from "../reducers/EquipmentSlice.ts";
-// import FieldSlice from "../reducers/FieldSlice.ts";
+import { configureStore } from "@reduxjs/toolkit";
 import VehicleSlice from "../reducers/VehicleSlice.ts";
 import StaffSlice from "../reducers/StaffSlice.ts";
 import CropSlice from "../reducers/CropSlice.ts";
 
 export const store = configureStore({
     reducer: {
-        // crop: CropSlice,
-        // equipment: EquipmentSlice,
-        // field: FieldSlice,
         crop: CropSlice,
         vehicle: VehicleSlice,
         staff: StaffSlice,
     }
 });
+
+// Define RootState type
+export type RootState = ReturnType<typeof store.getState>;
