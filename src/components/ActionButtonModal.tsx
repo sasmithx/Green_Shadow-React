5d5b27ae860f1e3a1804c39533d8cb5d5d9c6d66
Sasmith Manawadu <sasmithgit@gmail.com>
2025-01-16 08:36:43 +0530

    ActionButton Props Set

--- src/components/ActionButtonModal.tsx
+++ src/components/ActionButtonModal.tsx
@@ -1,4 +1,10 @@
-export const ActionButton = ({ id, label, style }) => (
+interface ActionButtonProps {
+    id: string;
+    label: string;
+    style?: React.CSSProperties;
+}
+
+export const ActionButton = ({ id, label, style }: ActionButtonProps) => (
     <button
         type="button"
         className="btn btn-dark px-4"
